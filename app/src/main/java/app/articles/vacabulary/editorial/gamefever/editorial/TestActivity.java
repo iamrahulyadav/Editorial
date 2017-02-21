5b5973939bc7deb2d1fec34395323071235f2c38
bunny1997 <priyank20patel@gmail.com>
2017-02-21 21:09:55 +0530

    Initial commit

---
+++ app/src/main/java/app/articles/vacabulary/editorial/gamefever/editorial/TestActivity.java
@@ -0,0 +1,29 @@
+package app.articles.vacabulary.editorial.gamefever.editorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+
+    public void btn1OnClick(View view) {
+    }
+
+    public void signInWithGoogle(View view) {
+    }
+
+    public void btn2OnClick(View view) {
+    }
+
+    public void btn3OnClick(View view) {
+    }
+
+    public void btn4OnClick(View view) {
+    }
+}
