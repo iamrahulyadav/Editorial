61936bfd38f7224844c710f8dc92e960b58e92b4
bunny1997 <priyank20patel@gmail.com>
2017-02-23 14:32:27 +0530

    editorial Class created

---
+++ app/src/main/java/app/articles/vacabulary/editorial/gamefever/editorial/EditorialFullInfo.java
@@ -0,0 +1,66 @@
+package app.articles.vacabulary.editorial.gamefever.editorial;
+
+/**
+ * Created by gamef on 23-02-2017.
+ */
+
+public class EditorialFullInfo {
+
+    private String editorialText ,editorialId ;
+    private EditorialGeneralInfo editorialGeneralInfo;
+
+    public EditorialFullInfo() {
+
+    }
+
+
+    public EditorialFullInfo(String editorialId) {
+        this.editorialId = editorialId;
+    }
+
+    public EditorialFullInfo(String editorialText, String editorialId, EditorialGeneralInfo editorialGeneralInfo) {
+        this.editorialText = editorialText;
+        this.editorialId = editorialId;
+        this.editorialGeneralInfo = editorialGeneralInfo;
+    }
+
+
+    public String getEditorialId() {
+        return editorialId;
+    }
+
+    public void setEditorialId(String editorialId) {
+        this.editorialId = editorialId;
+    }
+
+    public String getEditorialText() {
+        return editorialText;
+    }
+
+    public void setEditorialText(String editorialText) {
+        this.editorialText = editorialText;
+    }
+
+    public EditorialGeneralInfo geteditorialGeneralInfo() {
+        return editorialGeneralInfo;
+    }
+
+    public void seteditorialGeneralInfo(EditorialGeneralInfo editorialGeneralInfo) {
+        this.editorialGeneralInfo = editorialGeneralInfo;
+    }
+
+    public boolean checkEditorialText(){
+      /*check wether the fetched data is of the same id or not or the hext belong to this heading or noy
+      * true mean correct data
+      * false mean incorrect*/
+
+        if (editorialId.equalsIgnoreCase(editorialGeneralInfo.getEditorialID())){
+            return true;
+        }else{
+            return false;
+        }
+
+
+    }
+
+}
