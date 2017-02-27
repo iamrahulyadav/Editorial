4de9321810ee3cde2418a079777f3f4987827303
bunny1997 <priyank20patel@gmail.com>
2017-02-27 14:53:27 +0530

    about Activity Created and merged suceesfully

--- app/src/main/java/app/articles/vacabulary/editorial/gamefever/editorial/AboutActivity.java
+++ app/src/main/java/app/articles/vacabulary/editorial/gamefever/editorial/AboutActivity.java
@@ -2,6 +2,7 @@ package app.articles.vacabulary.editorial.gamefever.editorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.editorialAbout_activity_toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setSubtitle("About");
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+
     }
+
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
 }
