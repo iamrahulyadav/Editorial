73a01ec96965c34a95616b35b728f87766d4dfc0
bunny1997 <priyank20patel@gmail.com>
2017-02-27 15:30:47 +0530

    read full article feature integrated

--- app/src/main/java/app/articles/vacabulary/editorial/gamefever/editorial/EditorialFeedActivity.java
+++ app/src/main/java/app/articles/vacabulary/editorial/gamefever/editorial/EditorialFeedActivity.java
@@ -29,15 +29,15 @@ import java.text.BreakIterator;
 import java.util.Locale;
 
 public class EditorialFeedActivity extends AppCompatActivity implements
-        TextToSpeech.OnInitListener{
+        TextToSpeech.OnInitListener {
 
     String editorialText = "";
 
     private TextToSpeech tts;
 
-    TextView translateText ;
+    TextView translateText;
     private BottomSheetBehavior mBottomSheetBehavior;
-    public String selectedWord = "null" ;
+    public String selectedWord = "null";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,29 +52,27 @@ public class EditorialFeedActivity extends AppCompatActivity implements
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
 
 
-
         tts = new TextToSpeech(this, this);
         Intent i = getIntent();
         intialiseViewAndFetch(i);
 
-        translateText =(TextView)findViewById(R.id.editorial_feed_cardview_textview);
+        translateText = (TextView) findViewById(R.id.editorial_feed_cardview_textview);
 
         init(editorialText);
 
-        View bottomSheet = findViewById( R.id.editorial_activity_bottom_sheet );
+        View bottomSheet = findViewById(R.id.editorial_activity_bottom_sheet);
         mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
         mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
 
-        if(EditorialListActivity.isShowingAd) {
+        if (EditorialListActivity.isShowingAd) {
             initializeAds();
         }
-
 
 
     }
 
     private void intialiseViewAndFetch(Intent i) {
-    EditorialGeneralInfo editorialGeneralInfo = new EditorialGeneralInfo();
+        EditorialGeneralInfo editorialGeneralInfo = new EditorialGeneralInfo();
         editorialGeneralInfo.setEditorialID(i.getExtras().getString("editorialID"));
         editorialGeneralInfo.setEditorialDate(i.getExtras().getString("editorialDate"));
         editorialGeneralInfo.setEditorialHeading(i.getExtras().getString("editorialHeading"));
@@ -83,19 +81,16 @@ public class EditorialFeedActivity extends AppCompatActivity implements
         editorialGeneralInfo.setEditorialTag(i.getExtras().getString("editorialTag"));
 
         DBHelperFirebase dbHelper = new DBHelperFirebase();
-        dbHelper.getEditorialFullInfoByID(editorialGeneralInfo , this);
+        dbHelper.getEditorialFullInfoByID(editorialGeneralInfo, this);
 
-        TextView tv = (TextView)findViewById(R.id.editorial_heading_textview);
+        TextView tv = (TextView) findViewById(R.id.editorial_heading_textview);
         tv.setText(editorialGeneralInfo.getEditorialHeading());
-         tv = (TextView)findViewById(R.id.editorial_source_textview);
+        tv = (TextView) findViewById(R.id.editorial_source_textview);
         tv.setText(editorialGeneralInfo.getEditorialSource());
-         tv = (TextView)findViewById(R.id.editorial_date_textview);
+        tv = (TextView) findViewById(R.id.editorial_date_textview);
         tv.setText(editorialGeneralInfo.getEditorialDate());
-        tv = (TextView)findViewById(R.id.editorial_tag_textview);
+        tv = (TextView) findViewById(R.id.editorial_tag_textview);
         tv.setText(editorialGeneralInfo.getEditorialTag());
-
-
-
 
 
     }
@@ -120,23 +115,22 @@ public class EditorialFeedActivity extends AppCompatActivity implements
         }
     }
 
-    public void fetchEditorial(String id){
-      //call to fetch editorial object from firebase db
+    public void fetchEditorial(String id) {
+        //call to fetch editorial object from firebase db
     }
 
-    public void updateEditorialFetch(String editorial){
+    public void updateEditorialFetch(String editorial) {
 
-        TextView tv ;
-        tv=(TextView)findViewById(R.id.editorial_heading_textview);
+        TextView tv;
+        tv = (TextView) findViewById(R.id.editorial_heading_textview);
 //initialize heading
-        tv =(TextView)findViewById(R.id.editorial_date_textview);
+        tv = (TextView) findViewById(R.id.editorial_date_textview);
 //initialize date
-        tv =(TextView)findViewById(R.id.editorial_source_textview);
+        tv = (TextView) findViewById(R.id.editorial_source_textview);
 //initialize source
-        tv =(TextView)findViewById(R.id.editorial_text_textview);
+        tv = (TextView) findViewById(R.id.editorial_text_textview);
 //initialize text
         init(editorialText);
-
 
 
     }
@@ -167,55 +161,53 @@ public class EditorialFeedActivity extends AppCompatActivity implements
     private void onWordTap(String mWord) {
 
         speakOutWord(mWord);
-        Translation translation =new Translation(mWord);
+        Translation translation = new Translation(mWord);
         translation.fetchTranslation(this);
 
         translateText.setText(mWord);
-        selectedWord =mWord;
+        selectedWord = mWord;
     }
 
-    public  void updateTranslateText(Translation translation){
-        if(translation.word.equalsIgnoreCase(translateText.getText().toString().trim())) {
+    public void updateTranslateText(Translation translation) {
+        if (translation.word.equalsIgnoreCase(translateText.getText().toString().trim())) {
             translateText.setText(translation.word + " = " + translation.wordTranslation);
         }
     }
 
-    public void updateDictionaryText(final Dictionary dictionary){
-        TextView tv ;
-        tv=(TextView)findViewById(R.id.editorial_bottomsheet_meaning_textview);
+    public void updateDictionaryText(final Dictionary dictionary) {
+        TextView tv;
+        tv = (TextView) findViewById(R.id.editorial_bottomsheet_meaning_textview);
         tv.setText(dictionary.getWordMeaning());
-        tv=(TextView)findViewById(R.id.editorial_bottomsheet_partspeech_textview);
+        tv = (TextView) findViewById(R.id.editorial_bottomsheet_partspeech_textview);
         tv.setText(dictionary.getWordPartOfSpeech());
-        tv=(TextView)findViewById(R.id.editorial_bottomsheet_synonyms_textview);
-        String synonymstring="";
-        for(int i =0 ; i<dictionary.getWordsynonym().length ;i++) {
-            synonymstring=synonymstring+dictionary.getWordsynonym()[i] +" , ";
+        tv = (TextView) findViewById(R.id.editorial_bottomsheet_synonyms_textview);
+        String synonymstring = "";
+        for (int i = 0; i < dictionary.getWordsynonym().length; i++) {
+            synonymstring = synonymstring + dictionary.getWordsynonym()[i] + " , ";
         }
         tv.setText(synonymstring);
 
-        Button bt=(Button) findViewById(R.id.editorial_bottomSheet_add_button);
+        Button bt = (Button) findViewById(R.id.editorial_bottomSheet_add_button);
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatabaseHandler databaseHandler =new DatabaseHandler(EditorialFeedActivity.this);
+                DatabaseHandler databaseHandler = new DatabaseHandler(EditorialFeedActivity.this);
                 databaseHandler.addToDictionary(dictionary);
                 Toast.makeText(EditorialFeedActivity.this, "Word Added To Dictionary", Toast.LENGTH_SHORT).show();
-
-
 
 
             }
         });
     }
 
-    public void onDictionaryClick(View v){
+    public void onDictionaryClick(View v) {
         //Intent i =new Intent(this ,);
         mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
         openBottomSheet(true);
         Dictionary dictionary = new Dictionary(selectedWord);
-        dictionary.fetchWordMeaning(selectedWord ,this);
+        dictionary.fetchWordMeaning(selectedWord, this);
 
-        TextView tv = (TextView)findViewById(R.id.editorial_bottomsheet_heading_textview);
+        TextView tv = (TextView) findViewById(R.id.editorial_bottomsheet_heading_textview);
         tv.setText(translateText.getText());
 
 
@@ -223,12 +215,12 @@ public class EditorialFeedActivity extends AppCompatActivity implements
 
     private void openBottomSheet(boolean b) {
 
-        TextView tv ;
-        tv=(TextView)findViewById(R.id.editorial_bottomsheet_meaning_textview);
+        TextView tv;
+        tv = (TextView) findViewById(R.id.editorial_bottomsheet_meaning_textview);
         tv.setText("Loading...");
-        tv=(TextView)findViewById(R.id.editorial_bottomsheet_partspeech_textview);
+        tv = (TextView) findViewById(R.id.editorial_bottomsheet_partspeech_textview);
         tv.setText("Loading...");
-        tv=(TextView)findViewById(R.id.editorial_bottomsheet_synonyms_textview);
+        tv = (TextView) findViewById(R.id.editorial_bottomsheet_synonyms_textview);
 
         tv.setText("Loading...");
     }
@@ -254,7 +246,7 @@ public class EditorialFeedActivity extends AppCompatActivity implements
                     || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                 Log.e("TTS", "This Language is not supported");
             } else {
-               // btnSpeak.setEnabled(true);
+                // btnSpeak.setEnabled(true);
                 speakOutWord("");
             }
 
@@ -272,7 +264,8 @@ public class EditorialFeedActivity extends AppCompatActivity implements
 
     public void onGetEditorialFullInfo(EditorialFullInfo editorialFullInfo) {
 
-       init(editorialFullInfo.getEditorialExtraInfo().getEditorialText());
+        init(editorialFullInfo.getEditorialExtraInfo().getEditorialText());
+        editorialText =editorialFullInfo.getEditorialExtraInfo().getEditorialText();
         findViewById(R.id.editorialfeed_activity_progressbar).setVisibility(View.GONE);
 
     }
@@ -314,7 +307,7 @@ public class EditorialFeedActivity extends AppCompatActivity implements
     }
 
     private void onVacabularyClick() {
-        Intent i = new Intent(this ,VacabularyActivity.class);
+        Intent i = new Intent(this, VacabularyActivity.class);
         startActivity(i);
 
     }
@@ -333,29 +326,27 @@ public class EditorialFeedActivity extends AppCompatActivity implements
     }
 
     private void onAboutClick() {
-        Intent i = new Intent(this ,AboutActivity.class);
+        Intent i = new Intent(this, AboutActivity.class);
         startActivity(i);
     }
 
     private void onRefreashClick() {
 
 
-
     }
 
     public void addToDictionary(View view) {
-        DatabaseHandler databaseHandler =new DatabaseHandler(this);
-Dictionary dictionary =new Dictionary(selectedWord);
+        DatabaseHandler databaseHandler = new DatabaseHandler(this);
+        Dictionary dictionary = new Dictionary(selectedWord);
 
         TextView tv;
-        tv=(TextView)findViewById(R.id.editorial_bottomsheet_meaning_textview);
+        tv = (TextView) findViewById(R.id.editorial_bottomsheet_meaning_textview);
         dictionary.setWordMeaning(tv.getText().toString());
-        tv=(TextView)findViewById(R.id.editorial_bottomsheet_partspeech_textview);
+        tv = (TextView) findViewById(R.id.editorial_bottomsheet_partspeech_textview);
         dictionary.setWordPartOfSpeech(tv.getText().toString());
 
-        tv=(TextView)findViewById(R.id.editorial_bottomsheet_synonyms_textview);
-        dictionary.setWordsynonym(new String[]{ tv.getText().toString()});
-
+        tv = (TextView) findViewById(R.id.editorial_bottomsheet_synonyms_textview);
+        dictionary.setWordsynonym(new String[]{tv.getText().toString()});
 
 
         databaseHandler.addToDictionary(dictionary);
@@ -366,7 +357,7 @@ Dictionary dictionary =new Dictionary(selectedWord);
         return true;
     }
 
-    public void initializeAds(){
+    public void initializeAds() {
         MobileAds.initialize(getApplicationContext(), "ca-app-pub-8455191357100024~6634740792");
         AdView mAdView = (AdView) findViewById(R.id.editorialfeed_activity_adView);
         mAdView.setVisibility(View.VISIBLE);
@@ -374,5 +365,9 @@ Dictionary dictionary =new Dictionary(selectedWord);
 
         mAdView.loadAd(adRequest);
 
+    }
+
+    public void readFullArticle(View view) {
+        speakOutWord(editorialText);
     }
 }
