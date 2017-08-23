597b57c3e00163a2b0606f1baca22faad4ce8fec
bunny <gamefever20@gmal.com>
2017-08-23 23:31:26 +0530

    share link improvment done added native ads added theme option adding like
    update ads improvment v 1.6

--- app/src/main/java/app/articles/vacabulary/editorial/gamefever/editorial/EditorialListWithNavActivity.java
+++ app/src/main/java/app/articles/vacabulary/editorial/gamefever/editorial/EditorialListWithNavActivity.java
@@ -897,24 +897,17 @@ public class EditorialListWithNavActivity extends AppCompatActivity
 
 
 
-
-        /*Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
         sharingIntent.setType("text/plain");
 
         sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Download the app and Start reading");
         sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, EditorialListWithNavActivity.shareLink);
         startActivity(Intent.createChooser(sharingIntent, "Share via"));
-*/
 
-        int applicationNameId = this.getApplicationInfo().labelRes;
-        final String appPackageName = this.getPackageName();
-        Intent i = new Intent(Intent.ACTION_SEND);
-        i.setType("text/plain");
-        i.putExtra(Intent.EXTRA_SUBJECT, getString(applicationNameId));
-        String text = "Install this  Daily Editorial and improve you vocabulary: ";
-        String link = "https://play.google.com/store/apps/details?id=" + appPackageName;
-        i.putExtra(Intent.EXTRA_TEXT, text + "\n " + link);
-        startActivity(Intent.createChooser(i, "Share App :"));
+
+
+
+
 
 
     }
